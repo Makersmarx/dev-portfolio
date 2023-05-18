b6591bd7eb5ba0ee958511f8a7c39ff87b06c986
Christina Woodhams <cwoodhams89@gmail.com>
2023-05-18 18:49:43 -0400

    Sixth commit. Added hover and focus effect to anchor component.

--- src/components/Anchor.js
+++ src/components/Anchor.js
@@ -8,7 +8,7 @@ function Anchor ({
 }){
     const anchorClassNames = classNames(
         // Consistant styling
-        'text-amber-300 underline font-bold',
+        'text-amber-300 underline font-bold hover:underline-offset-4 focus:ring focus:ring-amber-300',
         className
     );
     return (
