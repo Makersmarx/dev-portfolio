8bbb793a9c1d28bb8407bfdcc42d9bbb7771d597
Rachel Spencer <rachelspencer688@gmail.com>
2023-05-26 14:58:21 -0400

    Header styling added, minus responsiveness

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,68 +1,75 @@
 import classNames from "classnames";
 import { Link } from 'react-scroll';
+import Panel from "./Panel";
 
 function Header (){
+    
     const headerLogo = "https://s24193.pcdn.co/wp-content/uploads/2016/07/entity-women-that-did-ada-lovelace-1320x720.jpg"
     return(
-        <header className="flex mx-8 my-5">
-            <div className="flex items-center justify-center h-12 w-12 rounded-full overflow-hidden">
-                <img src={headerLogo} alt={headerLogo} className="h-full w-full object-cover"/>
-            </div>
-            <span>
-                Ada Lovelace
-            </span>
-            <nav>
-                <ul>
-                    <li>
+        <header className="flex md:flex-row mx-12 my-5">
+            <Panel>
+                <div className="flex md:flex-row justify-start md:space-x-8">
+                    <div className="flex items-center justify-center h-12 w-12 rounded-full overflow-hidden">
+                        <img src={headerLogo} alt={headerLogo} className="h-full w-full object-cover"/>
+                    </div>
+                    <span className="flex items-center justify-center font-bold dark:text-indigo-500">
+                        Ada Lovelace
+                    </span>
+                </div>
+            </Panel>
+            <Panel>
+                <nav>
+                    <ul className="flex md:flex-row justify-end text-sm font-bold hover:cursor-pointer md:space-x-8 md:m-2 md:pt-2 tracking-wide dark:text-indigo-500">
+                        <li>
+                            <Link 
+                            to='intro' 
+                            smooth={true} 
+                            duration={500}
+                            >
+                            HOME
+                            </Link>
+                        </li>
+                        <li>
                         <Link 
-                        to='intro' 
-                        smooth={true} 
-                        duration={500}
-                        >
-                        HOME
-                        </Link>
-                    </li>
-                    <li>
-                    <Link 
-                        to='about me' 
-                        smooth={true} 
-                        duration={500}
-                        >
-                        ABOUT ME
-                        </Link>
-                    </li>
-                    <li>
-                        <Link 
-                        to='projects' 
-                        smooth={true} 
-                        duration={500}
-                        >
-                        PROJECTS
-                        </Link>
-                    </li>
-                    <li>
-                        <Link 
-                        to='contact' 
-                        smooth={true} 
-                        duration={500}
-                        >
-                        CONTACT
-                        </Link>
-                    </li>
-                    <li>
-                        <Link 
-                        to='testimonials' 
-                        smooth={true} 
-                        duration={500}
-                        >
-                        TESTIMONIALS
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
+                            to='about me' 
+                            smooth={true} 
+                            duration={500}
+                            >
+                            ABOUT ME
+                            </Link>
+                        </li>
+                        <li>
+                            <Link 
+                            to='projects' 
+                            smooth={true} 
+                            duration={500}
+                            >
+                            PROJECTS
+                            </Link>
+                        </li>
+                        <li>
+                            <Link 
+                            to='contact' 
+                            smooth={true} 
+                            duration={500}
+                            >
+                            CONTACT
+                            </Link>
+                        </li>
+                        <li>
+                            <Link 
+                            to='testimonials' 
+                            smooth={true} 
+                            duration={500}
+                            >
+                            TESTIMONIALS
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+            </Panel>
         </header>
-
     )
-}
+};
 
 export default Header;
--- src/components/Subtitles.js
+++ src/components/Subtitles.js
@@ -5,7 +5,7 @@ function Subtitles({
     className, 
     ...rest}){
     const subTitleClassNames = classNames(
-            'text-xl md:text-2xl font-bold text-left md: =m-2 md:pt-4 tracking-wide dark:text-indigo-500',
+            'text-xl md:text-2xl font-bold text-left md:m-2 md:pt-4 tracking-wide dark:text-indigo-500',
             className
     )
 
