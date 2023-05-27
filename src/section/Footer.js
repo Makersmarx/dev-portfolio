73d8841f74243d9ddbcf52a20ac59da423293258
Rachel Spencer <rachelspencer688@gmail.com>
2023-05-27 11:51:15 -0400

    Copy right tag line for footer complete

--- src/section/Footer.js
+++ src/section/Footer.js
@@ -2,9 +2,12 @@ import Panel from "../components/Panel"
 import Anchor from "../components/Anchor";
 
 function Footer () {
+    const feDevName = "Ada Lovelace"
+    const currentYear = new Date().getFullYear();
+    
 
     return (
-        <footer className="bg-black py-6 px-2">
+        <footer className="flex flex-col bg-black py-6 px-2">
             <Panel>
                 <div className="p-2">
                     <h1 className="text-lg py-6 text-white font-bold dark:text-indigo-500">SOCIAL</h1>
@@ -16,14 +19,17 @@ function Footer () {
                     </div>
                 </div>
                 <div className="text-white p-2">
-                <h1 className="text-lg py-6 text-white font-bold dark:text-indigo-500" >ADA LOVELACE</h1>
+                <h1 className="text-lg py-6 text-white font-bold dark:text-indigo-500" >{feDevName.toUpperCase()}</h1>
                 <p className="text-sm">A Frontend focused Web Developer building the Frontend of Websites and Web Applications that leads to the success of the overall product. </p>
                 </div>
             </Panel>
             <Panel>
-                <div className="py-6 px-2">
+                <div className="flex justify-center py-6 px-2">
                     <div className="border-t border-gray-300"></div>
-                    <div className="text-xs text-white pt-6">Copy Right</div>
+                    <div className="w-4 h-4 rounded-full bg-black border border-white flex justify-center items-center">
+                        <span className="text-white text-base">C</span>
+                    </div>
+                    <div className="text-xs text-white pl-2 flex items-center">Copy Right {currentYear}. Made by {feDevName}.</div>
                 </div>
                 
             </Panel>
