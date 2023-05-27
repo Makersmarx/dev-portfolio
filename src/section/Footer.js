551b6ee739d03cc4aa9fe6cd31acb292a5d548a6
Rachel Spencer <rachelspencer688@gmail.com>
2023-05-27 10:58:17 -0400

    font styling for Footer created

--- src/section/Footer.js
+++ src/section/Footer.js
@@ -4,10 +4,10 @@ import Anchor from "../components/Anchor";
 function Footer () {
 
     return (
-        <footer className="bg-black">
+        <footer className="bg-black py-6 px-2">
             <Panel>
-                <div>
-                    <h1 className="text-lg text-white p-2 font-bold dark:text-indigo-500">SOCIAL</h1>
+                <div className="p-2">
+                    <h1 className="text-lg py-6 text-white font-bold dark:text-indigo-500">SOCIAL</h1>
                     <div>
                         <Anchor>Linkedin</Anchor>
                         <Anchor>Github</Anchor>
@@ -15,13 +15,17 @@ function Footer () {
                         <Anchor>Youtube</Anchor>
                     </div>
                 </div>
-                <div className="text-white">
-                <h1 className="text-lg p-2 font-bold dark:text-indigo-500" >Ada Lovelace</h1>
-                <p>A Frontend focused Web Developer building the Frontend of Websites and Web Applications that leads to the success of the overall product. </p>
+                <div className="text-white p-2">
+                <h1 className="text-lg py-6 text-white font-bold dark:text-indigo-500" >ADA LOVELACE</h1>
+                <p className="text-sm">A Frontend focused Web Developer building the Frontend of Websites and Web Applications that leads to the success of the overall product. </p>
                 </div>
             </Panel>
             <Panel>
-                <div>Copy Right</div>
+                <div className="py-6 px-2">
+                    <div className="border-t border-gray-300"></div>
+                    <div className="text-xs text-white pt-6">Copy Right</div>
+                </div>
+                
             </Panel>
         </footer>
         
