932a50a4133d62a95bcfbb650a41707856acc38c
Rachel Spencer <rachelspencer688@gmail.com>
2023-05-28 11:13:58 -0400

    Footer responsivity and dark mode complete

--- src/section/Footer.js
+++ src/section/Footer.js
@@ -19,31 +19,31 @@ function Footer () {
       };
 
     return (
-        <footer className="flex flex-col bg-black py-6 px-2">
+        <footer className="flex flex-col bg-black py-6 px-2 dark:bg-slate-900">
             <Panel>
-                <div className="">
-                    <div className="md:flex-row p-2 text-white">
+                <div className="md:flex md:justify-between">
+                    <div className="md:order-last p-2 text-white">
                         <h1 className="text-lg py-6 text-white font-bold dark:text-indigo-500">SOCIAL</h1>
                         <div className="flex">
-                                <Anchor className="pr-6" href={hyperlinkToRender(hyperlink)[0]}><FaLinkedin size={24}/></Anchor>
-                                <Anchor className="pr-6" href={hyperlinkToRender(hyperlink)[1]}><FaGithub size={24}/></Anchor>
-                                <Anchor className="pr-6" href={hyperlinkToRender(hyperlink)[2]}><FaTwitter size={24}/></Anchor>
-                                <Anchor className="pr-6" href={hyperlinkToRender(hyperlink)[3]}><FaYoutube size={24}/></Anchor>
+                                <Anchor className="pr-6 focus:ring-0" href={hyperlinkToRender(hyperlink)[0]}><FaLinkedin size={24}/></Anchor>
+                                <Anchor className="pr-6 focus:ring-0" href={hyperlinkToRender(hyperlink)[1]}><FaGithub size={24}/></Anchor>
+                                <Anchor className="pr-6 focus:ring-0" href={hyperlinkToRender(hyperlink)[2]}><FaTwitter size={24}/></Anchor>
+                                <Anchor className="pr-6 focus:ring-0" href={hyperlinkToRender(hyperlink)[3]}><FaYoutube size={24}/></Anchor>
                         </div>
                     </div>
-                    <div className="md:flex-row text-white p-2">
+                    <div className="text-white p-2">
                         <h1 className="text-lg py-6 text-white font-bold dark:text-indigo-500" >{feDevName.toUpperCase()}</h1>
-                        <p className="text-sm pb-12">A Frontend focused Web Developer building the Frontend of Websites and Web Applications that leads to the success of the overall product. </p>
+                        <p className="text-sm pb-12 dark:text-slate-400">A Frontend focused Web Developer building the Frontend of Websites and Web Applications that leads to the success of the overall product. </p>
                     </div>
                 </div>
             </Panel>
             <Panel>
-                <div className="border-t border-gray-300"></div>
+                <div className="border-t border-gray-300 dark:border-cyan-500"></div>
                 <div className="flex justify-center py-6 px-2">
-                    <div className="w-4 h-4 rounded-full bg-black border border-white flex justify-center items-center">
-                        <span className="text-white text-base">C</span>
+                    <div className="w-4 h-4 rounded-full bg-black border border-white dark:border-indigo-500 flex justify-center items-center">
+                        <span className="text-white text-base dark:text-indigo-500">C</span>
                     </div>
-                    <div className="text-xs text-white pl-2 flex items-center">Copy Right {currentYear}. Made by {feDevName}.</div>
+                    <div className="text-xs text-white pl-2 flex items-center dark:text-indigo-500">Copy Right {currentYear}. Made by {feDevName}.</div>
                 </div>
             </Panel>
         </footer>
